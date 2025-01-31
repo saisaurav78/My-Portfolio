7609ab54cb3a3e3e2e7a35a5ced0ee98c60d7593
saisaurav <saisaurav78@gmail.com>
2025-01-31 20:04:30 +0530

    Added Hero Section

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+'use client'
+import { Link as ScrollLink } from "react-scroll"
+
+const Links = [
+    {
+    name:"home"
+},
+    {
+    name:"about"
+},
+    {
+    name:"journey"
+},
+    {
+    name:"work"
+    },
+    {
+    name:"work"
+    }
+]
+
+
+const Header = () => {
+  return (
+      <header className="w-full absolute py-8 xl:py-[48px] z-30">
+          <div className="container mx-auto">
+              <div>Logo</div>
+          </div>
+          <nav>
+              
+          </nav>
+    </header>
+  )
+}
+
+export default Header
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,31 +1,102 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation'
-import { Link as ScrollLink } from "react-scroll";
-
-
-
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+import { Link as ScrollLink } from 'react-scroll';
+import RotatingShape from './RotatingShape';
+import Header from './Header';
 
 const Hero = () => {
   return (
       <section className='h-[800px] relative bg-accent/10 xl:bg-white'>
-          <div className='container mx-auto h-full'>
-              <div className='relative z-20 h-full w-full xl:max-w-[768px] flex flex-col
-          items-center xl:items-start justify-center text-center xl:text-left pt-10'>
-                  <h1 className='h1'>
-                      <span className='text-accent'>I build Amazing </span> 
-                      <TypeAnimation preRenderFirstString={true}  sequence={['Websites', 2000, 'Apps', 2000]} speed={50} repeat={Infinity} wrapper='span' cursor={false}
-                      className='ml-2 xl:ml-4'/>
-                  </h1>
-                  <p className="lead"></p>
+          <Header/>
+      <div className='container mx-auto h-full'>
+        <div
+          className='relative z-20 h-full w-full xl:max-w-[768px] flex flex-col
+          items-center xl:items-start justify-center text-center xl:text-left pt-10'
+        >
+          <h1 className='h1 mb-2 max-w-[320px] xl:max-w-none'>
+            <span className='text-accent'>
+              I build and <span className='text-black'>Design</span> Amazing{' '}
+            </span>
+            <span className='inline-block'>
+              <TypeAnimation
+                preRenderFirstString={true}
+                sequence={['Websites', 2000, 'Apps', 2000]}
+                speed={50}
+                repeat={Infinity}
+                cursor={false}
+                className='inline'
+              />
+            </span>
+          </h1>
+          <p className='lead max-w-[476px] mb-7'>
+            Delivering powerful, custom websites that blend aesthetics with performance.
+          </p>
+          <ScrollLink to='contact' smooth>
+            <button className='btn btn-accent mb-8'>Contact Me</button>
+          </ScrollLink>
+        </div>
+        <div className='hidden xl:flex w-[55vw] h-[800px] absolute top-0 right-0 bg-accent'>
+          <div className='absolute w-[558px] h-[642px] bottom-8 z-40 left-[6.5vw]'>
+            <Image
+              alt='hero-image'
+              className='object-contain'
+              src={'/assets/hero/dev.png'}
+              fill
+              quality={100}
+              priority
+            />
           </div>
-          </div>    
-      
-      
-      </section>
-      
-  )
-}
+          <div className='hidden xl:flex absolute top-48 left-[4vw]'>
+            <Image src={'/assets/hero/arrow.svg'} width={160} height={160} alt='arrow' />
+          </div>
+          <div className='absolute top-[600px] left-[3vw]'>
+            <RotatingShape
+              direction={'left'}
+              duration={6}
+              content={
+                <Image
+                  src={'/assets/hero/shape-1.svg'}
+                  width={38}
+                  height={38}
+                  alt='rotating-shape'
+                />
+              }
+            />
+          </div>
+          <div className='absolute top-[240px] xl:left-[33vw]'>
+            <RotatingShape
+              direction={'right'}
+              duration={5}
+              content={
+                <Image
+                  src={'/assets/hero/shape-2.svg'}
+                  width={34}
+                  height={34}
+                  alt='rotating-shape'
+                />
+              }
+            />
+          </div>
+          <div className='absolute top-[480px] xl:left-[40vw]'>
+            <RotatingShape
+              direction={'left'}
+              duration={4}
+              content={
+                <Image
+                  src={'/assets/hero/shape-3.svg'}
+                  width={34}
+                  height={34}
+                  alt='rotating-shape'
+                />
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
