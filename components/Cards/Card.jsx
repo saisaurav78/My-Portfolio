0af4795fb354c7d0103c1e1d1546f30e8abd4bd3
saisaurav <saisaurav78@gmail.com>
2025-02-07 23:00:35 +0530

    Modified Cards, added tabs

--- components/Cards/Card.jsx
+++ components/Cards/Card.jsx
@@ -1,9 +1,43 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Card = () => {
+const Card = ({type,logoUrl,position,duration,description,company,institution,qualification,name,icon}) => {
   return (
-    <div>Card</div>
-  )
+    <div className='w-full h-[300px] overflow-hidden flex items-center sticky top-12'>
+      <div className='w-full h-[270px] border-accent/80 bg-[#f4ffff] rounded-[8px] '>
+        <div className='flex flex-col h-full'>
+          <div
+            className='h-[80px] xl:h-[68px] bg-white flex flex-col xl:flex-row justify-center xl:justify-between
+          items-center px-6 md:px-[84px] rounded-tl-[8px] rounded-tr-[8px]'
+          >
+            <div className='flex gap-2'>
+              <Image src={'/assets/journey/shape.svg'} width={16} height={16} alt='' />
+              <h3 className='text-lg font-semibold text-primary'>
+                {type === 'experience' ? position : type === 'education' ? qualification : duration}{' '}
+              </h3>
+              <p className='text-base'>
+                {type !== 'experience' && type !== 'education' ? null : duration}
+              </p>
+            </div>
+            <div className='flex-1 flex items-center justify-center xl:justify-start md:py-8 md:px-16'>
+              <div>
+                {type === 'skill' ? (
+                  <div>
+                    {' '}
+                    <div>{icon}</div>{' '}
+                  </div>
+                ) : (
+                  <div className='relative w-[300px] h-[38px] xl:h-[44px]'>
+                    <Image src={logoUrl} fill alt='' className='object-contain' />{' '}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card
--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -3,7 +3,7 @@ import Card from "./Card"
 import {Tabs,TabsContent,TabsList,TabsTrigger } from '@/components/ui/tabs'
 import { motion, AnimatePresence } from 'framer-motion'
 
-import { FaHtml5,FaCss3Alt,FaJs,FaReact,FaFigma,FaNodeJs,FaPython } from "react-icons/fa"
+import { FaHtml5,FaCss3Alt,FaJs,FaReact,FaFigma,FaNodeJs,FaPython, FaCss3 } from "react-icons/fa"
 
 const journey = [
     {
@@ -27,10 +27,58 @@ const journey = [
         position: 'Meta Frontend Developer Certification',
         duration: 'Nov 2024-Jan 2025',
     },
+    {
+        type: 'Skills',
+        name:'HTML',
+        icon:<FaHtml5/>,
+    },
+    {
+        type: 'Skills',
+        name:'CSS',
+        icon:<FaCss3/>,
+    },
+    {
+        type: 'Skills',
+        name:'JavaScript',
+        icon:<FaJs/>,
+    },
+    {
+        type: 'Skills',
+        name:'React.js',
+        icon:<FaReact/>,
+    },
+    {
+        type: 'Skills',
+        name:'Node.js',
+        icon:<FaNodeJs/>,
+    },
+    {
+        type: 'Skills',
+        name:'Python',
+        icon:<FaPython/>,
+    },
+    {
+        type: 'Skills',
+        name:'Figma',
+        icon:<FaFigma/>,
+    },
 ]
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <Tabs defaultValue="experience" className="w-full flex flex-col items-center">
+      <TabsList className='max-w-max mb-[30px]'>
+        <TabsTrigger>Experience</TabsTrigger>
+        <TabsTrigger>Education</TabsTrigger>
+        <TabsTrigger>My Skills</TabsTrigger>
+      </TabsList>
+      <TabsContent value='experience' className='w-full'>
+        <div>
+          {journey.filter((item) => item.type === 'experience').map((card, index) => {
+            return <Card key={index} {...card} />
+          })}
+        </div>
+      </TabsContent>
+    </Tabs>
   )
 }
 
