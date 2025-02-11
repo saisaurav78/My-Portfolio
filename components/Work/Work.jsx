545e17c7dbb8de960a600c78c24a081f0fc15fab
saisaurav <saisaurav78@gmail.com>
2025-02-11 22:30:38 +0530

    Updated Work section

--- components/Work/Work.jsx
+++ components/Work/Work.jsx
@@ -45,7 +45,31 @@ const Work = () => {
   };
   return (
     <section id='Work' className='pt-24 min-h-[1000px]'>
-     <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <Tabs defaultValue='all' className='w-full flex flex-col'>
+          <div className='flex flex-col xl:flex-row items-center xl:items-start xl:justify-between mb-[30px]' >
+            <AnimatedText text={'My Latest Work'} textStyles={'h2 mb-[30px] xl:mb-0'} />
+            <TabsList className='max-w-max h-full mb-[30px] flex flex-col md:flex-row gap-4 md:gap-0'>{tabData.map((item, index) => {
+              return <TabsTrigger onClick={()=>setTabValue(item.category)} value={ item.category} key={index} className='capitalize w-max min-w-[120px]' >{ item.category}</TabsTrigger>
+            }) }</TabsList>
+          </div>
+          
+          <TabsContent value={tabValue} className='w-full'> 
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-[30px]'>
+              <AnimatePresence>
+                {filterWork.slice(0, visibleItems).map((item, index) => (
+                  <motion.div key={index} initial={{opacity:0,y:20}} animate={{opacity:1,y:0}} transition={{duration:0.3}}> 
+                    <WorkItem {...item} />
+                  </motion.div>
+
+                ))}
+              </AnimatePresence>
+            </div>
+            {visibleItems < filterWork.length && (<div className='flex justify-center mt-12'><button onClick={loadMoreItems} className='btn btn-accent'>Load More</button></div>)}
+          </TabsContent>
+
+        </Tabs>
+      </div>
     </section>
   );
 };
